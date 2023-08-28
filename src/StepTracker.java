a5e66f7fc82ea5b9c3885990d953631859e493f8
Cicicai379 <caiyuxin379@gmail.com>
2023-08-28 09:57:49 +0800

    cici

---
+++ src/StepTracker.java
@@ -0,0 +1,48 @@
+public class StepTracker {
+    private int days;
+    private int activeDays;
+    private int totalSteps;
+    private int minActive;
+
+
+    public StepTracker(int x){
+        this. minActive=x;
+    }
+
+    public int getDays(){
+        return days;
+    }
+
+    public int getActiveDays(){
+        return activeDays;
+    }
+
+    public int activeDays() {
+        return activeDays;
+    }
+
+    public int getTotalSteps(){
+        return totalSteps;
+    }
+
+    public int getStepsForActiveDay(){
+        return minActive;
+    }
+
+
+
+    public double averageSteps(){
+        if (days==0) {
+            return 0.0;
+        }
+        return (double) totalSteps / days;
+    }
+
+    public void addDailySteps(int s){
+        days++;
+        totalSteps += s;
+        if (s >= minActive) {
+            activeDays++;
+        }
+    }
+}
